8266077fe1312d00762421a99176d02a57b7b3c2
edwinmuriithi <kabuimuriithi@gmail.com>
2023-12-23 17:07:11 +0300

    Initial Commit

---
+++ src/main/java/com/example/SecureFileHubPortal/SecureFileHubPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecureFileHubPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileHubPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileHubPortalApplication.class, args);
+	}
+
+}
